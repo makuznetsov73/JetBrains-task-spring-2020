0dced417e4bdb45cef67cc47f9fc617e78422f6d
Michael Kuznetsov <makuznetsov_3@edu.hse.ru>
2020-04-05 18:53:12 +0300

    Checking amount of command line args

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            ChainCall chainCall = new ChainCall(args[0]);
-            chainCall.reduceChainCall();
-            System.out.println(chainCall.getReducedChainStr());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        if (args.length != 0) {
+            try {
+                ChainCall chainCall = new ChainCall(args[0]);
+                chainCall.reduceChainCall();
+                System.out.println(chainCall.getReducedChainStr());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
